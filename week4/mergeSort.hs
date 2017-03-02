9797b4daf2f8e464319764d810f7c649a99d246e
Sudha Parimala <bmwaudi247@gmail.com>
2017-03-02 23:55:55 +0530

    Update mergeSort.hs

--- week4/mergeSort.hs
+++ week4/mergeSort.hs
@@ -4,3 +4,12 @@ merge xs [] = xs
 merge (x:xs) (y:ys)
     | (x <= y) = x:(merge xs (y:ys))
     | otherwise = y:(merge (x:xs) ys)
+
+
+mergesort :: [Int] -> [Int]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort xs = merge (mergesort (front xs)) (mergesort (back xs))
+	where 
+		front xs = take ((length xs) `div` 2) xs
+		back xs = drop ((length xs) `div`2) xs
