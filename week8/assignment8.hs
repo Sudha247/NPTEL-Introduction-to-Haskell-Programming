43ee6c6fcdd669da2f763e9a20cbc9fdc95150e4
Sudha Parimala <bmwaudi247@gmail.com>
2017-03-19 10:51:36 +0530

    Delete assignment8.hs

--- week8/assignment8.hs
+++
@@ -1,9 +0,0 @@
-main = do { 
-    line <- getLine ; 
-    if (line == "EOF")  
-        then return () ; 
-        else do  {
-            putStrLn ( reverse line ) ;
-            main  ;
-        }
-}
